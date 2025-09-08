6ccac11d45f13bbba884bb8b37e5a216181685d4
Enzo Petry <enzopetryort@gmail.com>
2025-09-08 20:33:42 -0300

    fix: Correção do endpoint de mensagens para produção

--- src/app/api/messages/route.js
+++ src/app/api/messages/route.js
@@ -1,24 +1,62 @@
 import { prisma } from "@/lib/prisma";
 import { auth } from "@/lib/auth";
 
-exports.dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
-exports.GET = async function (req) {
+export async function GET(req) {
 
-	const session = await auth.api.getSession({ headers: req.headers });
-	if (!session) {
-		return new Response(JSON.stringify({ error: "Unauthorized" }), { status: 401 });
+	try {
+		const session = await auth.api.getSession({ headers: req.headers });
+		if (!session) {
+			return new Response(
+				JSON.stringify({ error: "Unauthorized" }),
+				{
+					status: 401,
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+		}
+
+		const messages = await prisma.message.findMany({
+			orderBy: {
+				createdAt: "asc"
+			},
+			include: {
+				user: {
+					select: {
+						email: true,
+						name: true
+					}
+				}
+			},
+		});
+
+		const formattedMessages = messages.map(msg => ({
+			id: msg.id,
+			message: msg.text,
+			type: "user",
+			userId: msg.userId,
+			userName: msg.user.name || msg.user.email,
+			createdAt: msg.createdAt,
+		}));
+
+		return new Response(
+			JSON.stringify(formattedMessages),
+			{
+				status: 200,
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+	} catch (error) {
+		console.log("Erro ao buscar mensagens: ", error);
+		return new Response(JSON.stringify(
+			{
+				error: "Internal Server Error"
+			}), { status: 500 }
+		);
 	}
-
-	const messages = await prisma.message.findMany({
-		orderBy: { createdAt: "asc" },
-		include: { user: { select: { email: true, name: true } } },
-	});
-
-	return new Response(JSON.stringify(messages.map(msg => ({
-		message: msg.text,
-		type: "user",
-		userId: msg.userId,
-		userName: msg.user.name || msg.user.email,
-	}))), { status: 200 });
 }
